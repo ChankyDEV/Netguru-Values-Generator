168c54c83450333d83781008bee8a1560da8b275
Kamil_Koczan <kamil.koczan@naviparking.com>
2021-09-03 18:46:21 +0200

    Added needed packages

---
+++ lib/screens/sentences/sentences_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SentencesScreen extends StatelessWidget {
+  const SentencesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
